bde06dba5a3ba5de3c4af6144c4fb3952f46f50e
Mr.Blinky <4971163+MrBlinky@users.noreply.github.com>
2024-12-01 11:47:40 +0100

    Update comments

--- mickeymouse-save-patch.asm
+++ mickeymouse-save-patch.asm
@@ -1,15 +1,16 @@
 ;---------------------------------------------------------------
-;Save and reset patch for Mickey Mouse (UE), Mickey Mouse 2,
-;Hugo and Bugs Bunny Crazy Castle         by Mr.Blinky Nov.2024
+;Save and reset patch for Mickey Mouse (UE), Mickey Mouse II (J)
+;Hugo (E) (S), Bugs Bunny - Crazy Castle II (U) and Bugs Bunny -
+;Crazy Castle II from Bugs Bunny Collection (J) (V1.1) (S)
+;by Mr.Blinky Nov.-Dec.2024
 ;
 ;use RGBASM to build
 ;
 ;---------------------------------------------------------------
 
-
 IF DEF (MICKEY)
 
-;For mickey mouse 2
+;For Mickey Mouse II (J)
 DEF PASSWORD        EQU $C3A6   ;pointer to password var in ram
 DEF PASSWORD_UPDATE EQU $2B02   ;sub to draw password text
 DEF PASSWORD_RESET  EQU $2B5C   ;sub to reset password
@@ -17,7 +18,7 @@ DEF PASSWORD_TO_TXT EQU $2B11
 
 ELIF DEF (HUGO)
 
-;for Hugo (E)(S)
+;For Hugo (E)(S)
 DEF PASSWORD        EQU $C3A6   ;pointer to password var in ram
 DEF PASSWORD_UPDATE EQU $23AB   ;sub to draw password text
 DEF PASSWORD_RESET  EQU $2405   ;sub to reset password
@@ -25,14 +26,15 @@ DEF PASSWORD_TO_TXT EQU $23BA
 
 ELIF DEF (COLLECTION)
 
-;Bugs Bunny collection (J)(V1.1)(S)
+;For Bugs Bunny collection (J)(V1.1)(S)
 DEF PASSWORD        EQU $C3E6   ;pointer to password var in ram
 DEF PASSWORD_UPDATE EQU $58AD   ;sub to draw password text
 DEF PASSWORD_RESET  EQU $5907   ;sub to reset password
 DEF PASSWORD_TO_TXT EQU $58BC
 
 ELSE
-;For buggs bunny crazy castle
+
+;For Bugs Bunny - Crazy Castle II (U)
 DEF PASSWORD        EQU $C3A6   ;pointer to password var in ram
 DEF PASSWORD_UPDATE EQU $2B06   ;sub to draw password text
 DEF PASSWORD_RESET  EQU $2B60   ;sub to reset password
@@ -41,12 +43,12 @@ DEF PASSWORD_TO_TXT EQU $2B15
 ENDC
 
 ;-------------------------------------------------------------------------------
-;soft reset patch for Mickey mouse 2, Hugo and Buggs Bunny crazy castle
+;soft reset patch
 ;-------------------------------------------------------------------------------
 
 IF DEF (HUGO)
     SECTION "softreset",ROM0[$03D7]
-ELIF DEF (COLLECTION)    
+ELIF DEF (COLLECTION)
     SECTION "softreset",ROM0[$0396]
 ELSE
     SECTION "softreset",ROM0[$0388]
@@ -61,7 +63,7 @@ ENDC
 ; save patch
 ;-------------------------------------------------------------------------------
 
-DEF RAMG            EQU $0000
+DEF rRAMG           EQU $0000
 DEF _SRAM           EQU $A000
 
     SECTION "header cart type",ROM0[$0147]
@@ -105,7 +107,7 @@ ENDC
 ;---------------------------------------
 IF DEF (COLLECTION)
     SECTION "saveload",ROM0[$0080]
-ELSE    
+ELSE
     SECTION "saveload",ROM0[$0061]
 ENDC
 
@@ -142,7 +144,7 @@ load_checkcum:
     call copy_password
 load_end:
     xor  a          ;disable ram
-    ld   [RAMG],a
+    ld   [rRAMG],a
     jp   PASSWORD_UPDATE
 
 ;------------------------------
@@ -155,13 +157,13 @@ save_patch:
     call copy_password
     ld   [hl],c         ;store checksum
     xor  a              ;disable ram
-    ld   [RAMG],a
+    ld   [rRAMG],a
     jp   PASSWORD_TO_TXT
 
 ;--------------------------------------
 save_init:
     ld   a,$0A    	    ;enable SRAM
-    ld   [RAMG],a
+    ld   [rRAMG],a
     ld   hl,_SRAM       ;start of save ram
     ld   de,$0134       ;game title in header
     ld   b,15           ;game title length
